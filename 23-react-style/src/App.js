5813d3732ccc6c466dfb5bafa0e19e9000a6fba8
devjoowon <dev.joowon@gmail.com>
2024-03-25 14:31:18 +0900

    240325 react Sass

--- 23-react-style/src/App.js
+++ 23-react-style/src/App.js
@@ -2,16 +2,18 @@ import "./App.css";
 import AppCss from "./components/AppCss";
 import BasicCss from "./components/BasicCss";
 import ModuleCss from "./components/ModuleCss";
+import Sass from "./components/Sass";
 import StyledComp from "./components/StyledComp";
 
 function App() {
   return (
     <>
       <h1>react에 style 적용하기</h1>
-      <BasicCss />
+      {/* <BasicCss />
       <ModuleCss />
       <StyledComp />
-      <AppCss />
+      <AppCss /> */}
+      <Sass />
     </>
   );
 }
